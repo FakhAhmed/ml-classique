2e18dc08bffd10aa219284e3f5b92a3f7521aac5
Ahmed Fakhfakh <fakhfakhahmed45@gmail.com>
2026-06-25 15:26:35 +0200

    feat: découplage du modèle avec téléchargement dynamique depuis Google Cloud Storage

--- api/main.py
+++ api/main.py
@@ -3,15 +3,29 @@ import pandas as pd
 import xgboost as xgb
 from fastapi import FastAPI
 from pydantic import BaseModel
+from google.cloud import storage
+import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "src", "xgb_churn_model.json")
 
 app = FastAPI(title="Telecom Churn API")
 
-# 1. Chargement du modèle
+# --- NOUVELLE LOGIQUE DE TÉLÉCHARGEMENT GCS ---
+BUCKET_NAME = "modeles-ia-ml-classique-churn"
+MODEL_PATH_GCS = "v1/xgb_churn_model.json"
+LOCAL_MODEL_PATH = "/tmp/xgb_churn_model.json"
+
+print("⏳ Téléchargement du modèle depuis GCS...")
+client = storage.Client()
+bucket = client.bucket(BUCKET_NAME)
+blob = bucket.blob(MODEL_PATH_GCS)
+blob.download_to_filename(LOCAL_MODEL_PATH)
+print("✅ Modèle téléchargé avec succès !")
+
+# On charge le modèle depuis le fichier temporaire
 model = xgb.XGBClassifier()
-model.load_model(MODEL_PATH)
+model.load_model(LOCAL_MODEL_PATH)
 
 # NOUVEAU : On extrait la liste exacte des 30 colonnes attendues par le modèle
 EXPECTED_FEATURES = model.get_booster().feature_names
